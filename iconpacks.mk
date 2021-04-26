45515d58487e7aa1e2e9a8d73805d05d32036ff1
Aston-Martinn <advaithbhat9@gmail.com>
2021-04-26 08:57:48 +0000

    Introduce Custom Statusbar Signal & Wi-Fi icons [3/3]
    - Thanks to sis, acherus, iconicicons & cool_modules_69 for these beautiful icons.
    
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>
    Change-Id: I6544a1fb07e86b1347b37a9d55317d09d18e95c8
    Signed-off-by: Sagarrokade006 <Sagarrokade006@gmail.com>

---
+++ iconpacks.mk
@@ -0,0 +1,46 @@
+LOCAL_PATH := vendor/themes
+
+# Icon shape overlays
+PRODUCT_PACKAGES += \
+    IconPackCircularAndroidOverlay \
+    IconPackCircularLauncherOverlay \
+    IconPackCircularSettingsOverlay \
+    IconPackCircularSystemUIOverlay \
+    IconPackCircularThemePickerOverlay \
+    IconPackVictorAndroidOverlay \
+    IconPackVictorLauncherOverlay \
+    IconPackVictorSettingsOverlay \
+    IconPackVictorSystemUIOverlay \
+    IconPackVictorThemePickerOverlay \
+    IconPackSamAndroidOverlay \
+    IconPackSamLauncherOverlay \
+    IconPackSamSettingsOverlay \
+    IconPackSamSystemUIOverlay \
+    IconPackSamThemePickerOverlay \
+    IconPackKaiAndroidOverlay \
+    IconPackKaiLauncherOverlay \
+    IconPackKaiSettingsOverlay \
+    IconPackKaiSystemUIOverlay \
+    IconPackKaiThemePickerOverlay \
+    IconPackFilledAndroidOverlay \
+    IconPackFilledLauncherOverlay \
+    IconPackFilledSettingsOverlay \
+    IconPackFilledSystemUIOverlay \
+    IconPackFilledThemePickerOverlay \
+    IconPackRoundedAndroidOverlay \
+    IconPackRoundedLauncherOverlay \
+    IconPackRoundedSettingsOverlay \
+    IconPackRoundedSystemUIOverlay \
+    IconPackRoundedThemePickerOverlay
+
+# Statusbar Icons
+PRODUCT_PACKAGES += \
+    StrokeSignalOverlay \
+    SneakySignalOverlay \
+    XperiaSignalOverlay
+
+# Wi-Fi Icons
+PRODUCT_PACKAGES += \
+    StrokeWiFiOverlay \
+    SneakyWiFiOverlay \
+    XperiaWiFiOverlay
--- iconshapes.mk
+++ iconshapes.mk
@@ -9,3 +9,15 @@ PRODUCT_PACKAGES += \
     IconShapePebbleOverlay \
     IconShapeTaperedRectOverlay \
     IconShapeVesselOverlay
+
+# Statusbar Icons
+PRODUCT_PACKAGES += \
+    StrokeSignalOverlay \
+    SneakySignalOverlay \
+    XperiaSignalOverlay
+
+# Wi-Fi Icons
+PRODUCT_PACKAGES += \
+    StrokeWiFiOverlay \
+    SneakyWiFiOverlay \
+    XperiaWiFiOverlay
